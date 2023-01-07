5304dd82da55115ea44e14ecfa9442eab82eca53
syeddwahab1 <100497239+syeddwahab1@users.noreply.github.com>
2023-01-07 23:41:18 +0500

    Add files via upload

---
+++ variables.js
@@ -0,0 +1,24 @@
+ // store any data in variable and quote it
+ var a = "abc";
+ console.log(a);
+
+ // variable name cannot start with a number
+ // in var you can assign  it again and also overwrite it
+ var x = "abc";
+ var x = "xyz";
+ x = "pqr";
+
+ // you can also declare variable first and assign value later
+ var first, second;
+ first = 50;
+ console.log(first);
+
+ // let
+ // let is alse used same as variable but you cannot declare it again
+ // let allows overwriting value
+ let x = "abc";
+ x = "xyz";
+
+ // const
+ // in const you cannot decalre value again or overwrite ,the value assigned remains constant
+ const x = "xyz";
